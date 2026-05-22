28670ec4e14196dc63c0883ec5fa4a8a6b354f31
CodeWebWeaver <sanekparkhomovsky@gmail.com>
2026-05-22 18:35:27 +0300

    dialog improves

--- Assets/Script/Alex/Debug/Managers/DialogueManager.cs
+++ Assets/Script/Alex/Debug/Managers/DialogueManager.cs
@@ -92,15 +92,27 @@ public class DialogueManager : MonoBehaviour {
         ExitDialogueMode();
 
         _choiceSelector.OnChoiceSelected += OnChoiceSelected;
+    }
 
+    private void Subscribe() {
         dialogInputs.OnSubmitRequest += HandleSubmit;
         dialogInputs.OnNavigate += HandleNavigation;
     }
 
-    protected void OnDestroy() {
+    private void Unsubscribe() {
         dialogInputs.OnSubmitRequest -= HandleSubmit;
         dialogInputs.OnNavigate -= HandleNavigation;
+    }
 
+    private void OnEnable() {
+        Subscribe();
+    }
+
+    private void OnDisable() {
+        Unsubscribe();
+    }
+
+    protected void OnDestroy() {
         if (_choiceSelector != null)
             _choiceSelector.OnChoiceSelected -= OnChoiceSelected;
     }
